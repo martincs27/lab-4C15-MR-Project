b7a25a389667c4b0c18df1d898b2a19f19353786
Alumno <Alumno@LAB1509-20>
2017-04-28 17:16:46 -0500

    123456

--- Lab9GithubResponsable/src/com/tecsup/lab9/git/controller/StudentCtrl.java
+++ Lab9GithubResponsable/src/com/tecsup/lab9/git/controller/StudentCtrl.java
@@ -6,6 +6,8 @@ public class StudentCtrl {
 		System.out.println("HOLA REUS");
 		System.out.println("HOLA REUS");
 		System.out.println("HOLA REUS");
+		System.out.println("HOLA kktin");
+		System.out.println("HOLA kktin");
 //<<<<<<< HEAD
 		System.out.println("HOLA REUS");
 		System.out.println("HOLA REUS");
